97628f0c70015e95c749b6912b984a4617bed0c6
Daniil Starov <jazzzydan@hotmail.com>
2024-01-18 12:31:39 +0200

    uus reacti rakendus

---
+++ veebipood/src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src="https://upload.wikimedia.org/wikipedia/en/9/99/Nobe_GT100.jpg" className= "pilt" alt="Nobe GT100" />
+      <button className="nupp">NUPP</button>
+      <div>Tekst</div>
+      {/* <p></p>
+      <h1>Pealkiri1</h1>
+      <h2>Pealkiri2</h2>
+      <h3>Pealkiri3</h3> */}
+    </div>
+  );
+}
+
+export default App;
